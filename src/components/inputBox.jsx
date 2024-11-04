7d4909a15168ed98934b4a6d145369bcc261907a
rajeev <rajeevkumar25112002@gmail.com>
2024-11-04 18:00:55 +0530

    issue resolved

--- src/components/inputBox.jsx
+++ src/components/inputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 function InputBox({
     label,
     amount,
@@ -12,13 +12,16 @@ function InputBox({
     className = "",
 
 }){
+    const amountInputId = useId()
     return(
-        <div className="bg-white p-3 rounded-lg text-sm flex">
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-            <label className="text-black/40 mb-2 inline-block">
+            <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
             {label}
             </label>
+
             <input 
+            id={amountInputId}
             type="number"
             className="outline-none w-full bg-transparent py-1.5"
             placeholder="Amount"
@@ -29,16 +32,16 @@ function InputBox({
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
             <p className="text-black/40 mb-2 w-full">currency type</p>
+            
             <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
             value={selectCurrency}
             onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
-            disabled = {currencyDisable}
-            >
+            disabled = {currencyDisable}>
                 {currencyOptions.map((currency) => (
-                    <option value="usd" key={currency}>
+                    <option value={currency} key={currency}>
                         {currency}
                     
-                    usd</option>
+                    </option>
                 ))}
             </select>
             </div>
